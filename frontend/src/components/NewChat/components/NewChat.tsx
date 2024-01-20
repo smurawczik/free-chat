80f3891bdda587dd8bf2e645998f8fb551362cbf
smurawczik <sebastian.murawczik@gmail.com>
2024-01-20 16:23:07 +0100

    ignore envs

--- frontend/src/components/NewChat/components/NewChat.tsx
+++ frontend/src/components/NewChat/components/NewChat.tsx
@@ -1,10 +1,3 @@
-import { AddOutlined } from "@mui/icons-material";
-import { StyledIconButton } from "../../StyledIconButton";
-
 export const NewChat = () => {
-  return (
-    <StyledIconButton tooltip="Start a conversation">
-      <AddOutlined />
-    </StyledIconButton>
-  );
+  return null;
 };
